bfdb660ce8c253b66255b68f2e419d1e7588e702
Max Calvert-Minor <maxincalvertminor@gmail.com>
2025-05-28 04:53:49 +0000

    done!!!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
+//    PUT FUNCTION HERE
+//                 ↓↓↓↓
 string function = "6y^3*x^6";
+
+//        PUT VARIABLE OF DIFFERENTIATION HERE
+//                                         ↓↓↓
 Console.WriteLine(new Derivative(function, 'x'));
+
+// then type 'dotnet run' in the terminal to see the output
 
