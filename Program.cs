aa582fde93ef173a82bff12502994e2683c0f9f4
Max Calvert-Minor <maxincalvertminor@gmail.com>
2025-05-29 00:40:28 +0000

    googus

--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
 ﻿
 //    PUT FUNCTION HERE INSIDE THE QUOTES
 //                 ↓↓↓↓
-string function = "6x^2 + 3y";
+string function = "6x^2 + 3y^4";
 
 //        PUT VARIABLE OF DIFFERENTIATION HERE INSIDE THE QUOTES
 //                                         ↓↓↓
-Console.WriteLine(new Derivative(function, 'x'));
+Console.WriteLine(new Derivative(function, 'y'));
 
 // MAKE SURE EVERYTHING HAS PARENTHESE, THERE'S NO ORDER OF OPERATIONS
 
 // then type 'dotnet run' in the terminal to see the output
 // you can change tabs to take a look at the files
 
+// this is the terminal     ↓↓↓↓↓↓↓↓    type the command after the $
+//
+//       ____________________
+//      /                    \
+//     |                      |
+//     |     ●          ●     |
+//     |                      |
+//     |                      |
+//     |    _______________   |
+// ____|   | ____________  |  |________
+//     |   |______________/   |
+//     |                      | 
+//      \____________________/
+//         |               |
+//      ___|               |____
+//         
+//
+//
+//
